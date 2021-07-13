70e891631a6f198f218113d99e3ea60a8a02ff10
David Arayan <david.arayan12@gmail.com>
2021-07-13 17:16:44 +1000

    - Added azure shades

---
+++ generators/generate-azure-lut.js
@@ -0,0 +1,167 @@
+// shades of azure colours
+module.exports = [
+    {
+        name: "Azure",
+        rgb: [0, 128, 255]
+    },
+    {
+        name: "Azure Mist",
+        rgb: [240, 255, 255]
+    },
+    {
+        name: "Alice Blue",
+        rgb: [240, 248, 255]
+    },
+    {
+        name: "Light Sky Blue",
+        rgb: [135, 206, 250]
+    },
+    {
+        name: "Baby Blue",
+        rgb: [137, 207, 240]
+    },
+    {
+        name: "Sky Blue",
+        rgb: [135, 206, 235]
+    },
+    {
+        name: "Deep Sky Blue",
+        rgb: [0, 191, 255]
+    },
+    {
+        name: "Cerulean",
+        rgb: [0, 123, 167]
+    },
+    {
+        name: "Pale Azure",
+        rgb: [135, 211, 248]
+    },
+    {
+        name: "Green Blue",
+        rgb: [17, 100, 180]
+    },
+    {
+        name: "Maya Blue",
+        rgb: [115, 194, 251]
+    },
+    {
+        name: "Jordy Blue",
+        rgb: [138, 185, 241]
+    },
+    {
+        name: "Columbia Blue",
+        rgb: [185, 217, 235]
+    },
+    {
+        name: "Picton Blue",
+        rgb: [69, 177, 232]
+    },
+    {
+        name: "United Nations Blue",
+        rgb: [75, 146, 219]
+    },
+    {
+        name: "Cornflower Blue",
+        rgb: [100, 149, 237]
+    },
+    {
+        name: "Bleu De France",
+        rgb: [49, 140, 231]
+    },
+    {
+        name: "Dodger Blue",
+        rgb: [30, 144, 255]
+    },
+    {
+        name: "Brandeis Blue",
+        rgb: [0, 112, 255]
+    },
+    {
+        name: "True Blue",
+        rgb: [45, 104, 196]
+    },
+    {
+        name: "Tang Blue",
+        rgb: [0, 89, 207]
+    },
+    {
+        name: "Royal Blue",
+        rgb: [65, 105, 225]
+    },
+    {
+        name: "Celestial Blue",
+        rgb: [73, 151, 208]
+    },
+    {
+        name: "Vista Blue",
+        rgb: [124, 158, 217]
+    },
+    {
+        name: "Silver Lake Blue",
+        rgb: [93, 137, 186]
+    },
+    {
+        name: "Tufts Blue",
+        rgb: [62, 142, 222]
+    },
+    {
+        name: "Honolulu Blue",
+        rgb: [0, 118, 182]
+    },
+    {
+        name: "Air Force Blue",
+        rgb: [93, 138, 168]
+    },
+    {
+        name: "Steel Blue",
+        rgb: [70, 130, 180]
+    },
+    {
+        name: "French Blue",
+        rgb: [0, 114, 187]
+    },
+    {
+        name: "Lapis Lazuli",
+        rgb: [38, 97, 156]
+    },
+    {
+        name: "Lapis Lazuli",
+        rgb: [38, 97, 156]
+    },
+    {
+        name: "Spanish Blue",
+        rgb: [0, 112, 184]
+    },
+    {
+        name: "Argentinian Blue",
+        rgb: [108, 180, 238]
+    },
+    {
+        name: "Berkeley Blue",
+        rgb: [0, 50, 98]
+    },
+    {
+        name: "Celtic Blue",
+        rgb: [36, 107, 206]
+    },
+    {
+        name: "Polynesian Blue",
+        rgb: [34, 76, 152]
+    },
+    {
+        name: "Chefchaouen Blue",
+        rgb: [70, 143, 234]
+    },
+    {
+        name: "Sapphire",
+        rgb: [8, 37, 103]
+    },
+    {
+        name: "Delft Blue",
+        rgb: [31, 48, 94]
+    },
+    {
+        name: "Resolution Blue",
+        rgb: [0, 35, 135]
+    }
+];
